37f4344bc734c6228393d2ba49b1f8176e76b78c
Joe Monkila <jnkiwa25@gmail.com>
2024-09-13 10:37:22 +0100

    add url api

--- scripts/script.js
+++ scripts/script.js
@@ -173,7 +173,7 @@ function showAlert(message, time) {
 
 async function checkNumPlate(number) {
      const apiUrl = config.apiUrl
-    await fetch(`https://fakeapi-wqoi.onrender.com/cars?numero=` + number)
+    await fetch(`${apiUrl}/cars?numero=` + number)
         .then(response => response.json())
         .then(data => {
             closeVerifyModal();
@@ -188,7 +188,7 @@ async function verifyPlate() {
     let numero = document.getElementById('numeroVerify').value;
     const apiUrl = config.apiUrl
 
-    await fetch(`https://fakeapi-wqoi.onrender.com/cars?numero=` + numero)
+    await fetch(`${apiUrl}/cars?numero=` + numero)
         .then(response => response.json())
         .then(data => {
             closeVerifyModal();
@@ -211,7 +211,7 @@ async function addPlateInfos() {
         'proprietaire': proprietaire,
     };
 
-    await fetch(`https://fakeapi-wqoi.onrender.com/cars`, {
+    await fetch(`${apiUrl}/cars`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
